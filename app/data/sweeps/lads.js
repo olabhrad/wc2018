9275bb28182c4edc223031004495b0fa4d0cf965
Dylan Armstrong <olabhrad@tcd.ie>
2018-06-14 20:55:54 +0100

    Adding lads

---
+++ app/data/sweeps/lads.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    name: "Gus",
+    teams: ["Uruguay", "Sweden"]
+  },
+  {
+    name: "Ciaran",
+    teams: ["Peru", "Denmark"]
+  },
+  {
+    name: "Cathal",
+    teams: ["Argentina", "Iceland"]
+  },
+  {
+    name: "DK",
+    teams: ["Germany", "South Korea"]
+  },
+  {
+    name: "Dyl",
+    teams: ["Brazil", "Russia"]
+  },
+  {
+    name: "Tim",
+    teams: ["Croatia", "Japan"]
+  },
+  {
+    name: "Sean",
+    teams: ["Egypt", "Nigeria"]
+  },
+  {
+    name: "Paul",
+    teams: ["Spain", "Tunisia"]
+  },
+  {
+    name: "Olivia",
+    teams: ["Colombia", "Serbia"]
+  },
+{
+    name: "Thereesa",
+    teams: ["England", "Iran"]
+  },
+  {
+    name: "Glen",
+    teams: ["Belgium", "Panama"]
+  },
+  {
+    name: "Steph",
+    teams: ["Portugal", "Australia"]
+  },
+  {
+    name: "Graham",
+    teams: ["Switzerland", "Senegal"]
+  },
+  {
+    name: "Manus",
+    teams: ["Mexico", "Costa Rica"]
+  },
+  {
+    name: "Costy",
+    teams: ["Poland", "Morocco"]
+  },
+  {
+    name: "Al",
+    teams: ["France", "Saudi Arabia"]
+  }
+];
--- app/routes/sweepstake.js
+++ app/routes/sweepstake.js
@@ -1,10 +1,15 @@
 import Route from '@ember/routing/route';
 import aolSweepstake from 'wc2018/data/sweeps/aol';
+import ladsSweepstake from 'wc2018/data/sweeps/lads';
 
 export default Route.extend({
   model(path) {
     if (path.id === 'aol') {
       return aolSweepstake;
+    }
+    else
+    {
+    	return ladsSweepstake;
     }
   }
 });
